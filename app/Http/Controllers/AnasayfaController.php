a22400e7963b6c6503ef6a5f1d8a7f7a8751d82a
aykut <aykutatabek@hotmail.com>
2019-03-09 02:07:21 +0300

    2.4

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -7,6 +7,5 @@ use Illuminate\Http\Request;
 class AnasayfaController extends Controller
 {
    public function index(){
-       return view('anasayfa');
-   }
+         }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'AnasayfaController@index')->name('anasayfa');
 
+Route::get('/kategori/{slug_kategoriadi}','KategoriController@index')->name('kategori');
+
+Route::get('/urun/{slug_urunadi}','UrunController@index')->name('urun');
+
+Route::get('/sepet','SepetController@index')->name('sepet');
+
+Route::get('/odeme','OdemeController@index')->name('odeme');
+
+Route::get('/siparisler','SiparisController@index')->name('siparisler');
+
+Route::get('/siparisler/{id}','SiparisController@detay')->name('siparis');
+
+route::group(['prefix'=>'kullanici'],function (){
+Route::get('/oturumac','KullaniciController@giris_form')->name('kullanici.oturumac');
+
+Route::get('/kaydol','KullaniciController@kaydol_form')->name('kullanici.kaydol');
+});
