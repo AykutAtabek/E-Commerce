83617f0a274f906ca636e4c8d79dcd2b878e1de0
aykut <aykutatabek@hotmail.com>
2019-03-16 03:17:11 +0300

    2.5

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kategori;
 
 class AnasayfaController extends Controller
 {
    public function index(){
-         }
+       $kategoriler = Kategori::whereRaw('ust_id is null')->take(8)->get();
+       return view('anasayfa',compact('kategoriler'));
+   }
 }
